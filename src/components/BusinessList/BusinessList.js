7b81f4e0325557c6ec734db90de3e37e29a63c87
HerbertShin <hshin63@uwo.ca>
2021-07-09 00:49:31 -0400

    Added new components
    Created new components to fetch and display data from the Yelp api.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,17 +2,18 @@ import React from 'react';
 import "./BusinessList.css";
 import Business from "../Business/Business";
 
-
-class BusinessList extends React.Component{
-    render(){
-        return(
-            <div className="BusinessList">
-                {this.props.businesses.map(business=>{
-                    return <Business business={business}/>
-                })}
-            </div>
-        );
+class BusinessList extends React.Component {
+    render() {
+      return (
+        <div className="BusinessList">
+          {
+            this.props.businesses.map(business => {
+              return <Business business={business} key={business.id} />
+            })
+          }
+        </div>
+      );
     }
-}
-
-export default BusinessList;
+  }
+  
+  export default BusinessList;
